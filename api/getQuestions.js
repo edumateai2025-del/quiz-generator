613733518b25b5ebec4064633c9837e3b1ebd0e3
edumateai2025-del <edumateai2025@gmail.com>
2025-12-08 23:10:14 +0100

    Add API handler to return shuffled questions

--- api/getQuestions.js
+++ api/getQuestions.js
@@ -1,1 +1,6 @@
+import questions from '../data/questions.json';
 
+export default function handler(req, res) {
+  const shuffled = questions.sort(() => Math.random() - 0.5);
+  res.status(200).json(shuffled);
+}
